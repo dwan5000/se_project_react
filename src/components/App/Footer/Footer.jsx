6eebb0672122f0af65b0e086597ca71d398926f6
YOUR NAME <YOUR_EMAIL@DOMAIN.COM>
2025-07-21 19:37:59 -0500

    Finished!

---
+++ src/components/App/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "../Footer/Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__name">Developed by Name Surname</p>
+      <p className="footer__date">2025</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/App/ItemModal/ItemModal.jsx
@@ -0,0 +1,21 @@
+import "../ItemModal/ItemModal.css";
+import close from "../../../assets/whiteModalClose.svg";
+
+function ItemModal({ activeModal, onClose, card }) {
+  return (
+    <div className={`modal ${activeModal === "preview" && "modal_opened"}`}>
+      <div className="modal__content modal__content_type_image">
+        <button onClick={onClose} type="button" className="modal__close">
+          <img className="modal__close-button" src={close} alt="Close Button" />
+        </button>
+        <img src={card.link} alt="" className="modal__image" />
+        <div className="modal__footer">
+          <h2 className="modal__caption">{card.name}</h2>
+          <p className="modal__weather">Weather: {card.weather}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ItemModal;
